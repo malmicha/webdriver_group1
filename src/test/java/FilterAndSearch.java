e3ed83870259d80b8017e75efb223a7550df0167
Aleksandr Guido <iggelds@gmail,com>
2022-08-20 18:31:07 +0200

    Task4 - FilterAndSearch

--- src/test/java/FilterAndSearch.java
+++ src/test/java/FilterAndSearch.java
@@ -4,9 +4,12 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
 import java.time.Duration;
 import java.util.List;
 
@@ -15,11 +18,13 @@ public class FilterAndSearch {
     WebDriver webDriver;
 
     @BeforeMethod
-    public void SetUpDriver() {
+    public void SetUpDriverAndOpenWebpage() {
         if (webDriver == null) {
             WebDriverManager.chromedriver().setup();
             webDriver = new ChromeDriver();
         }
+        webDriver.get("https://www.amazon.com/s?k=gaming+chairs");
+        webDriver.manage().window().maximize();
     }
 
     @AfterTest
@@ -30,9 +35,6 @@ public class FilterAndSearch {
 
     @Test
     public void BrandsElementInTitle() {
-        webDriver.get("https://www.amazon.com/s?k=gaming+chairs");
-        webDriver.manage().window().maximize();
-
         WebElement brandLi = webDriver.findElement(By.xpath("(//*[@id=\"brandsRefinements\"]//li)[1]"));
         String brandName = brandLi.getAttribute("aria-label").toLowerCase();
         WebElement brandInput = brandLi.findElement(By.tagName("input"));
@@ -40,9 +42,57 @@ public class FilterAndSearch {
         webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
 
         List<WebElement> searchResults = webDriver.findElements(By.xpath("//*[@data-component-type=\"s-search-result\"]"));
-        for (WebElement el : searchResults) {
-            WebElement h2 = el.findElement(By.tagName("h2"));
+        for (WebElement element : searchResults) {
+            WebElement h2 = element.findElement(By.tagName("h2"));
             assert h2.getText().toLowerCase().startsWith(brandName);
+        }
+    }
+
+    @Test
+    public void PriceVerify() {
+        WebElement lowPrice = webDriver.findElement(By.xpath("//*[@id=\"low-price\"]"));
+        lowPrice.sendKeys("100");
+        WebElement highPrice = webDriver.findElement(By.xpath("//*[@id=\"high-price\"]"));
+        highPrice.sendKeys("150");
+        WebElement submitButton = webDriver.findElement(By.xpath("//*[@id=\"priceRefinements\"]//form"));
+        submitButton.submit();
+        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+
+        List<WebElement> searchResults = webDriver.findElements(By.xpath("//*[@data-component-type=\"s-search-result\"]"));
+        for (WebElement element : searchResults) {
+            try {
+                WebElement whole = element.findElement(By.className("a-price-whole"));
+                WebElement fraction = element.findElement(By.className("a-price-fraction"));
+                String result = whole.getText() + "." + fraction.getText();
+                double price = Double.parseDouble(result);
+                assert price >= 100.00;
+                assert price <= 150.00;
+            } catch (Exception e) {
+            }
+        }
+    }
+
+    @Test
+    public void PriceSorting() {
+        WebElement DropdownPrompt = new WebDriverWait(webDriver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@class=\"a-dropdown-prompt\"]")));
+        DropdownPrompt.click();
+        WebElement lowToHigh = webDriver.findElement(By.xpath("//*[@id=\"s-result-sort-select_1\"]"));
+        lowToHigh.click();
+        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+
+        List<WebElement> searchResults = webDriver.findElements(By.xpath("//*[@data-component-type=\"s-search-result\"]"));
+        double price = 0.00;
+        for (WebElement element : searchResults) {
+            try {
+                WebElement whole = element.findElement(By.className("a-price-whole"));
+                WebElement fraction = element.findElement(By.className("a-price-fraction"));
+                String result = whole.getText() + "." + fraction.getText();
+                double currentPrice = Double.parseDouble(result);
+                assert currentPrice >= price;
+                price = currentPrice;
+            } catch (Exception e) {
+            }
         }
     }
 }
